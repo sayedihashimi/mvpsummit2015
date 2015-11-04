e4886e1169aee0153580826f12f10db9bb6b1da9
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-11-04 10:13:31 -0800

    adding env sample

---
+++ multipleenv/src/MyConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp {
+    public class Program {
+        public void Main(string[] args) {
+            var keys = Environment.GetEnvironmentVariables();
+            foreach(string key in Environment.GetEnvironmentVariables().Keys) {
+                if (key.StartsWith("MVP") || key.StartsWith("Hosting:Environment")) {
+                    Console.WriteLine($"'{key}'='{Environment.GetEnvironmentVariable(key)}'");
+                }
+            }
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
